87fab5678d7f222f1d1a63cf6fd30c417520e599
Michael Biggs <mbiggs@pixio.com>
2017-02-21 16:53:28 -0700

    updated podfile and project settings

--- Example/Pods/Target Support Files/Pods-UIAlertController-BetterConstructor-UIAlertController-BetterConstructor/Pods-UIAlertController-BetterConstructor-UIAlertController-BetterConstructor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_UIAlertController_BetterConstructor_UIAlertController_BetterConstructor : NSObject
-@end
-@implementation PodsDummy_Pods_UIAlertController_BetterConstructor_UIAlertController_BetterConstructor
-@end
---
+++ Example/Pods/Target Support Files/UIAlertController-BetterConstructor/UIAlertController-BetterConstructor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIAlertController_BetterConstructor : NSObject
+@end
+@implementation PodsDummy_UIAlertController_BetterConstructor
+@end
